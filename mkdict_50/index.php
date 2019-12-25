e109e6058a4e7d55b371003c36d61a4c3c243b9d
Alexey Romanenko <arom@ngs.ru>
2019-12-25 16:25:54 +0700

    FIX: forgot to add the main file

---
+++ mkdict_50/index.php
@@ -0,0 +1,169 @@
+<?php 
+
+include("inc/config.php");
+require_once("inc/db.php");
+include("inc/login.php");
+include("inc/anketa.php");
+include("inc/collector.php");
+require_once("inc/log.php");
+
+session_start();
+
+if(isset($_POST['i_lang']) && in_array($_POST['i_lang'], $valide_lang)){
+	$_SESSION['i_lang'] = $_POST['i_lang'];
+}else{
+	if(!isset($_SESSION['i_lang']) ||  !in_array($_SESSION['i_lang'], $valide_lang)){
+		$_SESSION['i_lang'] = $valide_lang[0];
+	}
+}
+
+
+
+if(!isset($_SESSION['test_id']) || ($_SESSION['test_id']<13 && $_SESSION['test_id']>19)){
+	echo "<html><body onLoad=\"window.location='/mkdict_50/redir.php?t=new543FR'\"></body></html>";
+	return;
+}
+
+if($_SESSION['test_id'] == 36) $_SESSION['i_lang'] = 'fr'; // special case
+
+include("inc/lang_".$_SESSION['i_lang'].".php");
+
+?>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+<table width="100%" height="100%" border="0" style="border-spacing:0px;">
+<tr height="30%" style="background-color: #12bbad">
+	<td width="50%"><h1 style="color:#FFFFFF; padding-left:30%">Dictionnaires des associations verbales du Français</h1></td>
+	<td width="50%"><img src="../imgs/NSU_logo_English_Green.png" style="width:50%"></td>
+</tr>
+<tr height="70%">
+	<td valign="center" colspan=2>
+		<center>
+
+<?php
+if(!isset($_SESSION['stage'])){
+	$_SESSION['stage']=0; // login
+}
+$err = "";
+
+write_log("stage={$_SESSION['stage']}");
+
+if($_SESSION['stage'] == 0){
+	if($_POST['key']){
+		$err = check_key($_POST['key']);
+		if($err == ''){
+			$_SESSION['stage'] = 1; // anketa
+			$_SESSION['key'] = $_POST['key'];
+			$_SESSION['test_lang'] = $Last_key_lang;
+//			$_SESSION['test_id'] = $Test_id;
+			write_log("auth OK");
+		}else{
+			login_form($err);
+			write_log("auth failed: ".$err);
+		}
+	}else{
+		login_form('');
+		write_log("Print login form");
+	}
+}
+
+if($_SESSION['stage'] == 1){
+	$age = "";
+	$sex = "";
+	$edu = "";
+	$lang = "";
+	$spec = "";
+	$city = "";
+//	var_dump($_POST);
+	if(isset($_POST['submit'])){
+		unset($_POST['submit']);
+		if($_POST['age'] == ''){
+			$age=""; $err .= $locale['age_empty']." ";
+		}else{
+			if($_POST['age'] <= 90 || $_POST['age'] >= 3){	
+				$age = $_POST['age'];
+			}
+			else{
+				$age="";
+				$err .= $locale['age_failed'];
+			}
+		}
+		if($_POST['sex'] == 'M')	$sex = true;
+		else						$sex = false;
+		if($_POST['spec'] != '')	$spec = $_POST['spec']; 
+		else						$err .= "<br>".$locale['spec_failed'];
+		if($_POST['city'] != '')	$city = $_POST['city']; 
+		else						$err .= "<br>".$locale['city_failed'];
+		
+		if($_POST['lang'] == 'oo'){
+			if($_POST['other'] == '') $err .= "<br>".$locale['lang_failed'];
+			else					  $lang = $_POST['other'];
+		}else
+			$lang = $_POST['lang']; 
+		$edu = $_POST['edu'];
+		
+		if($err == ''){
+			$_SESSION['stage'] = 2; //collection
+			$_SESSION['age'] = intval($age);
+			$_SESSION['sex'] = $sex;
+			$_SESSION['edu'] = intval($edu);
+			$_SESSION['spec'] = $locale[$spec];
+			$_SESSION['city'] = $city;
+			$_SESSION['lang'] = $lang;
+			$_SESSION['reg'] = intval($_POST['reg']);
+			write_log("Anketa filled out OK");
+		}else{
+			write_log("Anketa filled out with errors: ".$err);
+			anketa_form($err);
+		}
+	}else{
+		write_log("Print anketa");
+		anketa_form('');
+	}
+}
+$repeat = 2;
+
+if($_SESSION['stage'] == 2){
+        if(isset($_SESSION['times']) && $_SESSION['times'] > 0){
+		$_SESSION['times']++;
+		if($_SESSION['times'] > $repeat) $_SESSION['stage'] = 3;
+	}else
+		$_SESSION['times'] = 1;
+	if(isset($_POST['words'])){
+		preg_match_all("|([0-9]+):([^;]+);|", $_POST['words'], $out, PREG_PATTERN_ORDER);
+		write_log("Saved words. failed={$_POST['failed']} count=".count($out[0]));
+		if($_POST['failed'] == 0){
+			$err = db_save_words($_POST['count']);
+			// check for error
+			write_log("Saved words ".(($err == 0)? "OK":"failed ".$err));
+		}
+		if($_SESSION['stage'] == 2){ //participate up to $repeat times
+			collect_form();
+			write_log("Print collection form. ".$_SESSION['times']);
+		}
+	}else{
+		collect_form();
+		write_log("Print collection form (1st times). ".$_SESSION['times']);
+	}
+}
+
+if($_SESSION['stage'] == 3){
+	thank_form();
+	write_log("Print thanks form");
+	session_unset();
+	session_destroy();
+}
+
+?>
+		</center>
+	</td>
+</tr>
+</table>
+</body>
+</html>
+
