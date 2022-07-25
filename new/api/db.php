f0cc2ebdf396c798d7b61669f9bd0e737ce3f7ce
Alexey Romanenko <arom@ngs.ru>
2022-07-25 10:54:30 +0700

    fix of updated writing eo to œ

--- new/api/db.php
+++ new/api/db.php
@@ -47,7 +47,7 @@ function db_right_dict($test, $chr){
 						$search .= "AND  lower(dict.word) SIMILAR  TO '((un|une|le|la|les) )*(a|à|â){$rest}%' ";
 					}else
 					if(strtolower($chr[0]) == 'o'){
-						$search .= "AND  lower(dict.word) SIMILAR  TO '((un|une|le|la|les) )*(o|ô){$rest}%' ";
+						$search .= "AND  lower(dict.word) SIMILAR  TO '((un|une|le|la|les) )*(o|ô|œ){$rest}%' ";
 					}else
 					if(strtolower($chr[0]) == 'c'){
 						$search .= "AND  lower(dict.word) SIMILAR  TO '((un|une|le|la|les) )*(c|ç){$rest}%' ";
@@ -65,7 +65,7 @@ function db_right_dict($test, $chr){
 					}else
 					if(strtolower($chr[0]) == '?'){
 						$search .= "AND  lower(dict.word)  NOT SIMILAR  TO ".
-							"'(1|2|3|4|5|6|7|8|9|0|a|à|â|b|c|ç|d|e|é|è|ê|f|g|h|i|î|j|k|l|m|n|o|ô|p|q|r|s|t|u|û|v|w|x|y|z)%' ";
+							"'(1|2|3|4|5|6|7|8|9|0|a|à|â|b|c|ç|d|e|é|è|ê|f|g|h|i|î|j|k|l|m|n|o|ô|œ|p|q|r|s|t|u|û|v|w|x|y|z)%' ";
 					}else { 
 
 						$search .= "AND lower(dict.word) similar to '((un|une|le|la|les) )*".strtolower($chr)."%' ";
@@ -190,11 +190,11 @@ function db_back_dict($test, $methodParam,$filter){
 			} else
 			if (strtolower($methodParam[0]) == 'u') {
 				$search_char .= "AND  (lower(resp.word_inv) SIMILAR TO '((un|une|le|la|les) )*(u|û){$rest}%'  AND ".
-				"lower(resp.word) not similar to '(un|une) (1|2|3|4|5|6|7|8|9|0|a|à|â|b|c|ç|d|e|é|è|ê|f|g|h|i|î|j|k|l|m|n|o|ô|p|q|r|s|t|v|w|x|y|z)%') ";
+				"lower(resp.word) not similar to '(un|une) (1|2|3|4|5|6|7|8|9|0|a|à|â|b|c|ç|d|e|é|è|ê|f|g|h|i|î|j|k|l|m|n|o|ô|œ|p|q|r|s|t|v|w|x|y|z)%') ";
 			} else
 			if (strtolower($methodParam[0]) == 'l') {
 				$search_char .= "AND  (lower(resp.word_inv) similar to '((un|une|le|la|les) )*l{$rest}%' AND ".
-				"lower(resp.word) not similar to '(la|le|les) (1|2|3|4|5|6|7|8|9|0|a|à|â|b|c|ç|d|e|é|è|ê|f|g|h|i|î|j|k|m|n|o|ô|p|q|r|s|t|u|û|v|w|x|y|z)%') ";
+				"lower(resp.word) not similar to '(la|le|les) (1|2|3|4|5|6|7|8|9|0|a|à|â|b|c|ç|d|e|é|è|ê|f|g|h|i|î|j|k|m|n|o|ô|œ|p|q|r|s|t|u|û|v|w|x|y|z)%') ";
 			} else
 			if (strtolower($methodParam[0]) == '?') {
 				$search_char .= "AND  lower(resp.word_inv)  NOT SIMILAR  TO ".
